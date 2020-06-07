50c59b00d0ff2e501c01334113c5266cf54f4150
LaloLoop <gt.silva.e@gmail.com>
2020-06-07 20:10:43 +0000

    Display status code after body

--- fetch/main.go
+++ fetch/main.go
@@ -6,10 +6,14 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "https://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -21,5 +25,6 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
+		fmt.Printf("\nStatus code %v\n", resp.Status)
 	}
 }
